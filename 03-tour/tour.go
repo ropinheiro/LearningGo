af52dfb340004c16a5762c889eed1c8b78fb637f
Rui Pinheiro <rui.m.s.oliveira.pinheiro@gmail.com>
2022-08-18 18:07:47 +0100

    add (tour): use of math/rand package

--- 03-tour/tour.go
+++ 03-tour/tour.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"time"
 )
 
 func main() {
     fmt.Println("Hello, Tour!")
+
   	fmt.Println("The time is", time.Now())
+
+    rand.Seed(int64(time.Now().Nanosecond()))
+    fmt.Println("My favorite number is", rand.Intn(10))
 }
