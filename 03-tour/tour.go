c22a55a0694a96dae38ad208f72827d7cbe0f0ce
Rui Pinheiro <rui.m.s.oliveira.pinheiro@gmail.com>
2022-09-02 15:51:26 +0100

    fix (tour): use spaces instead of tabs + fix identation

--- 03-tour/tour.go
+++ 03-tour/tour.go
@@ -64,7 +64,7 @@ func showOffTypes() {
     MaxInt uint64     = 1<<64 - 1
     z      complex128 = cmplx.Sqrt(-5 + 12i)
   )
- 	fmt.Printf("> Type: %T Value: %v\n", ToBe, ToBe)
-	fmt.Printf("> Type: %T Value: %v\n", MaxInt, MaxInt)
-	fmt.Printf("> Type: %T Value: %v\n", z, z)
+  fmt.Printf("> Type: %T Value: %v\n", ToBe, ToBe)
+  fmt.Printf("> Type: %T Value: %v\n", MaxInt, MaxInt)
+  fmt.Printf("> Type: %T Value: %v\n", z, z)
 }
